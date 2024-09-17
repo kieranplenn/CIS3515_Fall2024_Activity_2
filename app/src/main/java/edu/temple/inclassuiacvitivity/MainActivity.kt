1ab848f72d0afbfde2c3bfe4757cd84b449700fd
kieranplenn <tuk25850@temple.edu>
2024-09-17 12:46:06 -0400

    Added onSelect functionality and consolidated some previous code!

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.temple.inclassuiacvitivity
 
 import android.os.Bundle
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,16 +15,22 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
-        val numberArray = Array(20){ 5 * (it + 1) }
+        val numberArray = Array(20) { 5 * (it + 1) }
         //numberArray.toTypedArray()
 
         /* TODO Step 3: Create adapter to display items from array in Spinner */
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray)
-        spinner.adapter = adapter
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray)
 
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                displayTextView.setTextSize(spinner.selectedItem.toString().toFloat())
+            }
+        }
+
+
 
     }
 }
